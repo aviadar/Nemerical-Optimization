e9de9d7661f1d3f31248139ad297bf62360c6e2b
AVIADAR <aviad222@gmail.com>
2021-06-19 08:19:09 +0300

    updates to plots

--- Code/src/utils.py
+++ Code/src/utils.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from matplotlib.collections import PatchCollection
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from matplotlib.patches import Polygon
 import numpy as np
 
 
@@ -52,7 +55,7 @@ def plot_contours_paths(f, x_hist):
     CS = ax1.contour(X, Y, Z, 30)
     ax1.clabel(CS, inline=True, fontsize=10)
     for iteration, x in enumerate(x_hist.T):
-        ax1.plot(x[0], x[1], 'o', color=colors[iteration], label='path'+str(iteration))
+        ax1.plot(x[0], x[1], 'o', color=colors[iteration], label='path' + str(iteration))
 
     plt.xlabel('x1')
     plt.ylabel('x2')
@@ -63,50 +66,58 @@ def plot_contours_paths(f, x_hist):
 
 def plot_qp(f, x_hist):
     fig = plt.figure()
-    ax = plt.axes(projection='3d')
+    fig.suptitle('QP convergence')
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+
+    poly3d = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    ax.add_collection3d(Poly3DCollection(poly3d, alpha=0.5))
 
     x0 = np.linspace(0, 1, 100)
     x1 = np.linspace(0, 1, 100)
 
     X, Y = np.meshgrid(x0, x1)
-    # Z = np.maximum(0, 1 - X - Y)
-    Z = 1-X-Y
+    Z = 1 - X - Y
     C = np.zeros(X.shape)
     for i in range(X.shape[0]):
         for j in range(X.shape[1]):
             C[i, j] = f.evaluate(np.array([[X[i, j]], [Y[i, j]], [Z[i, j]]]))
 
-    valid_indicies = np.ones(X.shape)
-    valid_indicies[X < 0] = np.nan
-    valid_indicies[Y < 0] = np.nan
-    valid_indicies[Z < 0] = np.nan
-    X[np.isnan(valid_indicies)] = np.nan
-    Y[np.isnan(valid_indicies)] = np.nan
-    Z[np.isnan(valid_indicies)] = np.nan
-    # ax.contour3D(X,Y,Z,cmap=valid_indicies )
-    ax.plot_surface(X, Y, Z, alpha=0.5)
-    # plt.pcolor(X,Y,Z)
     ax.plot(x_hist[0, :], x_hist[1, :], x_hist[2, :], '-o', label='path')
-    ax.plot(x_hist[0, -1], x_hist[1, -1], x_hist[2, -1], marker='^', markerfacecolor='yellow', markersize=12, label='final x')
+    ax.plot(x_hist[0, -1], x_hist[1, -1], x_hist[2, -1], marker='^', markerfacecolor='yellow', markersize=12,
+            label='final x')
 
-    plt.xlabel('x1')
-    plt.ylabel('x2')
+    plt.xlabel('x0')
+    plt.ylabel('x1')
+    ax.set_zlabel('x2')
     ax.legend()
 
-    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 2, 2)
+
+    poly = np.array([[1, 0], [0, 1], [0, 0]])
+    patch = [Polygon(poly, True)]
+    collection = PatchCollection(patch, alpha=0.5, label='feasible region')
+    ax1.add_collection(collection)
+
     CS = plt.contour(X, Y, C, 10)
     plt.clabel(CS, inline=True, fontsize=10)
 
     plt.plot(x_hist[0, :], x_hist[1, :], '-o', label='path')
     plt.plot(x_hist[0, -1], x_hist[1, -1], marker='^', markerfacecolor='yellow', markersize=12, label='final x')
 
-    plt.xlabel('x1')
-    plt.ylabel('x2')
+    plt.xlabel('x0')
+    plt.ylabel('x1')
     plt.legend()
     plt.show()
 
 
 def plot_lp(f, x_hist):
+    fig, ax = plt.subplots()
+
+    poly = np.array([[1, 0], [2, 0], [2, 1], [0, 1]])
+    patch = [Polygon(poly, True)]
+    collection = PatchCollection(patch, alpha=0.5, label='feasible region')
+    ax.add_collection(collection)
+
     x0 = np.linspace(-0.1, 2.1, 1000)
     x1 = np.linspace(-0.1, 1.1, 1000)
 
@@ -116,23 +127,13 @@ def plot_lp(f, x_hist):
         for j in range(X.shape[1]):
             C[i, j] = f.evaluate(np.array([[X[i, j]], [Y[i, j]]]))
 
-    valid_indicies = np.ones(X.shape)
-    X_plus_Y = X + Y
-    valid_indicies[X_plus_Y < 1] = np.nan
-    valid_indicies[Y > 1] = np.nan
-    valid_indicies[X > 2] = np.nan
-    valid_indicies[Y < 0] = np.nan
-
-    plt.pcolor(X, Y, valid_indicies, label='feasible region')
     CS = plt.contour(X, Y, C, 30)
     plt.clabel(CS, inline=True, fontsize=10)
 
-    plt.plot(x_hist[0, :], x_hist[1, :], '-o', label='path')
-    plt.plot(x_hist[0, -1], x_hist[1, -1], marker='^', markerfacecolor='yellow', markersize=12, label='final x')
+    ax.plot(x_hist[0, :], x_hist[1, :], '-o', label='path')
+    ax.plot(x_hist[0, -1], x_hist[1, -1], marker='^', markerfacecolor='yellow', markersize=12, label='final x')
 
     plt.legend()
     plt.xlabel('x0')
     plt.ylabel('x1')
-    plt.title('LP convergence')
     plt.show()
-
