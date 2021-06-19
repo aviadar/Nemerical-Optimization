a459c5f1fadfc68eed57d89a913b8516b0c2f716
AVIADAR <aviad222@gmail.com>
2021-06-19 08:23:21 +0300

    updates to plots

--- Code/tests/test_constrained_min.py
+++ Code/tests/test_constrained_min.py
@@ -1,9 +1,8 @@
 import numpy as np
 import unittest
 from Code.src.constrained_min import interior_pt
-from Code.src.unconstrained_min import line_search
 from Code.tests.examples import ConstrainedQuadraticFunction, ConstrainedLPFunction
-from Code.src.utils import final_report, plot_contours_paths, plot_val_hist, plot_qp, plot_lp
+from Code.src.utils import final_report, plot_qp, plot_lp
 
 
 class TestConstrainedMin(unittest.TestCase):
@@ -17,6 +16,7 @@ class TestConstrainedMin(unittest.TestCase):
         success, last_x, val_hist, x_hist = interior_pt(func=constrained_qp, x0=x0, obj_tol=obj_tol,
                                                         param_tol=param_tol, max_inner_loops=max_inner_loops)
 
+        final_report(success, last_x)
         plot_qp(constrained_qp.f0, x_hist)
 
     def test_lp(self):
@@ -29,6 +29,7 @@ class TestConstrainedMin(unittest.TestCase):
         success, last_x, val_hist, x_hist = interior_pt(func=constrained_lp, x0=x0, obj_tol=obj_tol,
                                                         param_tol=param_tol, max_inner_loops=max_inner_loops)
 
+        final_report(success, last_x)
         plot_lp(constrained_lp.f0, x_hist)
 
 
